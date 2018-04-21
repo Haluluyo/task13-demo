782a79d3e941dd3c17357059c2bae2b3649e012e
halulu <lulijuan_00@163.com>
2018-04-21 13:28:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-app.get('/getNews', function(req, res){
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+var url = require('url')
 
-	var news = [
-		"第11日前瞻：中国冲击4金 博尔特再战200米羽球",
-		"正直播柴飚/洪炜出战 男双力争会师决赛",
-		"女排将死磕巴西！郎平安排男陪练模仿对方核心",
-		"没有中国选手和巨星的110米栏 我们还看吗？",
-		"中英上演奥运金牌大战",
-		"博彩赔率挺中国夺回第二纽约时报：中国因对手服禁药而丢失的奖牌最多",
-		"最“出柜”奥运？同性之爱闪耀里约",
-		"下跪拜谢与洪荒之力一样 都是真情流露"
-	]
-	var data = [];
-	for(var i=0; i<3; i++){
-		var index = parseInt(Math.random()*news.length);
-		data.push(news[index]);
-		news.splice(index, 1);
-	}
+http.createServer(function(req, res){
+  var pathObj = url.parse(req.url, true)
 
+  switch (pathObj.pathname) {
+    case '/getNews':
+      var news = [
+        "第11日前瞻：中国冲击4金 博尔特再战200米羽球",
+        "正直播柴飚/洪炜出战 男双力争会师决赛",
+        "女排将死磕巴西！郎平安排男陪练模仿对方核心"
+        ]
+      res.setHeader('Content-Type','text/json; charset=utf-8')
+      if(pathObj.query.callback){
+        res.end(pathObj.query.callback + '(' + JSON.stringify(news) + ')')
+      }else{
+        res.end(JSON.stringify(news))
+      }
 
-	var cb = req.query.callback;
-	if(cb){
-		res.send(cb + '('+ JSON.stringify(data) + ')');
-	}else{
-		res.send(data);
-	}
+      break;
 
-	
-})
+    default:
+      fs.readFile(path.join(__dirname, pathObj.pathname), function(e, data){
+        if(e){
+          res.writeHead(404, 'not found')
+          res.end('<h1>404 Not Found</h1>')
+        }else{
+          res.end(data)
+        }
+      }) 
+  }
+}).listen(8080)
